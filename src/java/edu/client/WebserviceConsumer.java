416341521923266a8186d397a0551817d898d3aa
Yamil Elías <yamileliassoto@gmail.com>
2016-10-28 19:00:30 -0600

    Added needed files for hangman

--- src/java/edu/client/WebserviceConsumer.java
+++ src/java/edu/client/WebserviceConsumer.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2016 Yamil Elías <yamileliassoto@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package edu.client;
@@ -19,6 +30,11 @@ import org.json.*;
  */
 public class WebserviceConsumer {
 
+    /**
+     * Retrieve random word from supplying web service.
+     * @param wsURL
+     * @return 
+     */
     public String getWord(String wsURL){
         String word = "";
         
@@ -36,6 +52,11 @@ public class WebserviceConsumer {
         return word;
     }
     
+    /**
+     * Get the contents that the provided URL have to show.
+     * @param theUrl
+     * @return 
+     */
     private String getUrlContents(String theUrl) {
         StringBuilder content = new StringBuilder();
 
